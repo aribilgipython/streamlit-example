589190e730ef85c0f5aa75662ab1dfc132df7180
aribilgipython <105739227+aribilgipython@users.noreply.github.com>
2023-08-13 10:36:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-import plotly
+import plotly.express as px
 st.title("İlk Cloud Streamlit Projem")
 
 btc=yf.download("BTC-USD","2008-01-01","2023-08-12")
