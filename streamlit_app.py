ff83b0975b1ee2309c490275d9b58f5e175a1207
aribilgipython <105739227+aribilgipython@users.noreply.github.com>
2023-08-13 10:43:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,9 @@ st.title("İlk Cloud Streamlit Projem")
 btc=yf.download("BTC-USD","2008-01-01","2023-08-12")
 btc=btc.reset_index()
 
+fig = px.line(btc, x='Date', y="Close")
+#fig.show()
 
-
+st.plotly_chart(fig)
 
 st.table(btc)
