2f4b39e2404ce32401ba8cdb92bb5c105fc6c2d9
aribilgipython <105739227+aribilgipython@users.noreply.github.com>
2023-08-13 09:53:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit as st
-st.set_page_config(layout="wide")
-st.header("Ana Sayfa")
-from functions import *
-st.subheader("Günün Randevuları")
-gununrandevu()
+
+st.title("İlk Cloud Streamlit Projem")
